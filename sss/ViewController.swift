6ecbd74e5933d12d3a65133d8346e2f955f90f6f
山崎拡史 <ghost427@yamazakihiromushinoMacBook-Pro.local>
2016-12-16 23:20:08 +0900

    first commit

--- sss/ViewController.swift
+++ sss/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var number:Int! = 0
+    @IBOutlet weak var label:UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func botan (){
+         number = number + 1
+        label.text = String(number)
+    }
 }
 
